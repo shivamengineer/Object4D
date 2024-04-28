8af7ba88bdf9448293c52dc4044afaf781868b4f
Shivam Engineer <34991071+shivamengineer@users.noreply.github.com>
2024-04-27 22:47:16 -0400

    Update Object2.java
    planning out updates

--- Objects4D/src/Object2.java
+++ Objects4D/src/Object2.java
@@ -11,9 +11,10 @@ public final class Object2 extends ObjectSecondary {
     private int numVertices;
 
     /**
-     *
+     * change to points
      */
     private Line2[] lines;
+    //private Point2[] points;
 
     /**
      *
@@ -21,14 +22,16 @@ public final class Object2 extends ObjectSecondary {
     private int dimensions;
 
     /**
-     *
+     * adjust to use points instead
      */
     public Object2() {
         this.dimensions = Constants.DIMENSIONS;
         this.numVertices = Constants.DIMENSIONS;
         this.lines = new Line2[this.numVertices];
+        //this.points = new Point2[this.numVertices];
         for (int i = 0; i < this.numVertices; i++) {
             this.lines[i] = new Line2();
+            //this.points[i] = new Point2();
         }
     }
 
@@ -40,7 +43,7 @@ public final class Object2 extends ObjectSecondary {
                 * this.numVertices : "violation of enough point coordinates";
 
         //set dimensions
-
+        //replace lines with points
         boolean init = false;
         int[] last = new int[Constants.DIMENSIONS];
         int lines = 0;
