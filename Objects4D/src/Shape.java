c66d5de73af3347f7c310252002f5c52e624fe54
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-05-12 22:33:39 -0400

    added new secondary methods to interface

--- Objects4D/src/Shape.java
+++ Objects4D/src/Shape.java
@@ -5,4 +5,25 @@
  */
 public interface Shape extends ShapeKernel {
 
+    /**
+     *
+     * @param time
+     * @param translateDistance
+     */
+    void translateFrame(int time, Integer[] translateDistance);
+
+    /**
+     *
+     * @param time
+     * @param newCoords
+     */
+    void setFramePosition(int time, Integer[] newCoords);
+
+    /**
+     *
+     * @param time
+     * @return position at time
+     */
+    Integer[] getPositionAtTime(int time);
+
 }
